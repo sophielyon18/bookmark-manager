8524459bd9c2b5e6e1467b3edbf41402f7a261e7
Petra Boldi <petraboldi@Petras-MacBook-Air.local>
2021-04-28 17:24:34 +0200

    sequelize migrations

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require('./queries.js')
+
+app.use(express.urlencoded({ extended: false }))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('./index')
 })
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
