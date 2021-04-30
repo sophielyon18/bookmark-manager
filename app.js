204cc4636221da79c64d1a1be5cb5cfa56ef9942
Sophie Lyon <sophielyon@MacBook-Pro-8.home>
2021-04-30 15:12:52 +0100

    updates

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const methodOverride = require('method-override')
 
-const bookmarkController = require('./controllers/bookmark.js')
-// const addBookmarkController = require('./controllers/newBookmark')
-app.use('/bookmarks', bookmarkController)
-// app.use('/bookmarks', addBookmarkController)
-
-const Bookmark = require('./models').Bookmark
-
-app.post('/bookmarks', async function (req, res) {
-  Bookmark.create({url: req.body.url})
-  await req.body.url.save()
-  console.log(req.body.url)
-  const bookmarks = await Bookmark.findAll()
-  res.render("bookmarks/index", { bookmarks: bookmarks })
-})
-
-app.use(express.urlencoded({ extended: false }))
-
-app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('./index')
-})
+const bookmarkController = require('./controllers/bookmark.js')
 
+app.use('/bookmarks', bookmarkController)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -4,8 +4,31 @@ const router = express.Router()
 const Bookmark = require('../models').Bookmark
 
 router.get('/', async function (req, res) {
-    const bookmarks = await Bookmark.findAll() 
-    res.render('bookmarks/index', { bookmarks: bookmarks})
+  const bookmarks = await Bookmark.findAll()
+  res.render("bookmarks/index", { bookmarks: bookmarks })
+})
+
+router.get('/:bookmarkId/edit', async function (req, res) {
+  const bookmark = await Bookmark.findOne({ where: { id: req.params.bookmarkId } })
+  res.render("bookmarks/edit", { bookmark: bookmark })
+})
+
+router.post('/', async function (req, res) {
+  await Bookmark.create({url: req.body.url})
+  const bookmarks = await Bookmark.findAll()
+  res.render("bookmarks/index", { bookmarks: bookmarks })
+})
+
+router.delete('/:bookmarkId', async function (req, res) {
+  await Bookmark.destroy({where: { id: req.params.bookmarkId } })
+  const bookmarks = await Bookmark.findAll()
+  res.render("bookmarks/index", { bookmarks: bookmarks })
+})
+
+router.put('/:bookmarkId', async function (req, res) {
+  await Bookmark.update({url: req.body.url}, { where: { id: req.params.bookmarkId } })
+  const bookmarks = await Bookmark.findAll()
+  res.render("bookmarks/index", { bookmarks: bookmarks })
 })
 
 module.exports = router
